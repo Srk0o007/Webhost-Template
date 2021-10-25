b7fdfcdfaf58dc3f8434e1bcb6d850c2e8de1486
Mohd Shahrukh <khan.shahrukh156@gmail.com>
2021-10-25 19:07:43 +0530

    First commit

---
+++ js/homepage.js
@@ -0,0 +1,50 @@
+
+!(function($) {
+    "use strict";
+  
+    // Skills section
+    $('.skills-content').waypoint(function() {
+      $('.progress .progress-bar').each(function() {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      });
+    }, {
+      offset: '80%'
+    });
+  
+    // Porfolio isotope and filter
+    $(window).on('load', function() {
+      var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item'
+      });
+  
+      $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+  
+        portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+        aos_init();
+      });
+  
+      // Initiate venobox (lightbox feature used in portofilo)
+      $(document).ready(function() {
+        $('.venobox').venobox({
+          'share': false
+        });
+      });
+    });
+  
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  
+    $(window).on('load', function() {
+      aos_init();
+    });
+  
+  })(jQuery);
